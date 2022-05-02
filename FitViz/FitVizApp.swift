c698cd07d69722f56813d9e8f0d57a7a4515df9b
mike-griffin <mike_griffin@comcast.net>
2022-05-02 15:01:02 -0700

    Initial commit

---
+++ FitViz/FitVizApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitVizApp.swift
+//  FitViz
+//
+//  Created by Mike Griffin on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FitVizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ FitViz/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitViz
+//
+//  Created by Mike Griffin on 5/2/22.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
